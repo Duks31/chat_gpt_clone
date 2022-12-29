58c40a9d296867f40196d74c093e6f9c015768d6
Duks31 <chidubemjan31@gmail.com>
2022-12-29 14:21:42 +0100

    update

--- chatGPT.py
+++ chatGPT.py
@@ -1,20 +1,23 @@
+# importing libraries
 import openai
 import gradio as gr
-from api_secret import API_KEY
+from api_secret import API_KEY # importing api key from api_secrets
 
-openai.api_key = API_KEY
+# Get your API key from openai 
+openai.api_key = 'your api key'
 
 start_sequence = "\nAI:"
 restart_sequence = "\nHuman:"
 prompt = "what is your name"
 
 
+#main prompt gotten from the openai playground 
 def openai_create(prompt):
 
     response = openai.Completion.create(
     model="text-davinci-003",
     prompt=prompt,
-    temperature=0.9,
+    temperature=0.5,
     max_tokens=150,
     top_p=1,
     frequency_penalty=0,
@@ -25,7 +28,7 @@ def openai_create(prompt):
     return response.choices[0].text
 
 
-
+# the main chatGPT 
 def chatgpt_clone(input, history):
     history = history or []
     s = list(sum(history, ()))
@@ -36,8 +39,8 @@ def chatgpt_clone(input, history):
     return history, history
 
 
+#Gradio implementation 
 block = gr.Blocks()
-
 
 with block:
     gr.Markdown("""<h1><center>GPT CLONE</center></h1>""")
